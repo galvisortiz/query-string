26c0a168813c072df692cffa307f24c37c4de3d1
galvis Ortiz <galvis-ortiz@hotmail.com>
2020-09-03 22:46:41 -0500

    se pone la h mayuscula

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     if(nombre){        
         res.send(`<h1>Hola ${nombre}!</h1>`);
     }else{
-        res.send(`<h1>hola desconocido</h1>`); 
+        res.send(`<h1>Hola desconocido!</h1>`); 
     }    
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));

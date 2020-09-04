3ea139257e255fa54a185c1f352d9a06483edfbe
galvis Ortiz <galvis-ortiz@hotmail.com>
2020-09-03 20:04:21 -0500

    se modifica if

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const { query } = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
-    if(req.query.name){
-        res.send('hola ' + req.query.nombre);
+    const nombre = req.query.nombre;
+    if(nombre){        
+        res.send(`<h1>hola ${nombre}!</h1>`);
     }else{
-        res.send("Hola desconocido!");  
-    }
-    
+        res.send(`<h1>hola desconocido</h1>`); 
+    }    
 });
-
 app.listen(3000, () => console.log('Listening on port 3000!'));
+
+
+
+
+// if(req.query.name){
+        
+//     res.send(`<h1>hola ${req.query.name}!</h1>`);
+//     console.log
+// }else{
+//     res.send(`<h1>hola desconocido</h1>`); 
+// }
+
+// });
+// const nombre = req.query.nombre;

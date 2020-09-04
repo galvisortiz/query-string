304fafbabe3af4952d4c6bf5c110f8e77218a171
galvis Ortiz <galvis-ortiz@hotmail.com>
2020-09-03 19:23:21 -0500

    se agrega if

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { query } = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hola ' + req.query.nombre);
+    if(req.query.name){
+        res.send('hola ' + req.query.nombre);
+    }else{
+        res.send("Hola desconocido!");  
+    }
+    
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
